63ad5719987c9920f09aa74cc819c1856f0162f5
Marco <marcoflaco@gmail.com>
2022-10-22 00:18:40 +0200

    add list

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import VueGoogleAutocomplete from 'vue-google-autocomplete'
+import  "./main.css";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
--- src/views/RideListing/RideList.vue
+++ src/views/RideListing/RideList.vue
@@ -1,50 +1,29 @@
 <template>
-    <b-container>
-        <b-row align-h="center">
-            <h2>Available Rides</h2>
-        </b-row>
-            <b-card v-if="rideList!== null" class="card">
-                <b-table small responsive :items="rideList" :fields="fields" ref="selectableTable" selectable select-mode="single" @row-selected="onRowSelected">
-                    <template slot="userId" slot-scope="row">
-                        {{ row.value.userId }}
-                    </template>
-                    <template slot="fullName" slot-scope="row">
-                        {{ row.value.fullName }}
-                    </template>
-                    <template slot="fullName" slot-scope="row">
-                        {{ row.value.phoneNumber }}
-                    </template>
-                    <template slot="pickupPoint.placeAddress" slot-scope="row">
-                        {{ row.value.pickupPoint.placeAddress }}
-                    </template>
-                    <template slot="destination.placeAddress" slot-scope="row">
-                        {{ row.value.destinationPoint.placeAddress }}
-                    </template>
-                    <template slot="tripStartTime" slot-scope="row">
-                        {{ row.value.tripStartTime }}
-                    </template>
-                    <template slot="offeredSeats" slot-scope="row">
-                        {{ row.value.offeredSeats }}
-                    </template>
-                    <template slot="vehicleNumber" slot-scope="row">
-                        {{ row.value.vehicleNumber }}
-                    </template>
-                </b-table>
-                <b-row align-h="center">
-                    <b-button variant="success" @click="join">Join</b-button>
-                </b-row>
-            </b-card>
-            <b-card v-else-if="rideList === null" class="card">
-                No Data
-            </b-card>
-    </b-container>
+    <b-container fluid class="p-4 bg-dark">
+        <div>
+    <h2 style="color=white">Drivers</h2>
+    <b-list-group style="max-width: 300px;">
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar class="mr-3"></b-avatar>
+        <span class="mr-auto">Driver</span>
+        <b-badge>5</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="primary" text="BV" class="mr-3"></b-avatar>
+        <span class="mr-auto">Emission label</span>
+        <b-badge>12</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="info" src="https://placekitten.com/300/300" class="mr-3"></b-avatar>
+        <span class="mr-auto">Credits to earn</span>
+        <b-badge>9</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="success" icon="people-fill" class="mr-3"></b-avatar>
+        <span class="mr-auto">Seats availabels</span>
+        <b-badge>7</b-badge>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</b-container>
 </template>
-<script src="./ride-list.js"></script>
-<style scoped>
-.card {
-    margin-top:1%;
-    background-color: #ccdde0 !important;
-    opacity: 0.95;
-    padding: 0px;
-}
-</style>
