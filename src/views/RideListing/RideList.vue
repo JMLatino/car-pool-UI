e2d9733b6d130ff126f29b58c0b8ca9422ea8135
Juan Besselse <juan.besselse.navas@nl.abnamro.com>
2022-10-22 14:06:45 +0200

    Added list call

--- src/views/RideListing/RideList.vue
+++ src/views/RideListing/RideList.vue
@@ -25,16 +25,21 @@ export default {
       list: []
     }
   },
-  methods: {
-    populate() {
-      let path = "https://lchaj9w7t3.execute-api.eu-west-1.amazonaws.com/dev/trips"
-      axios.get(path)
+  created(){
+     axios.get(path)
         .then(function (response) {
             this.list = response;
         })
         .catch(function () {
           alert('Find Ride API failure')
         })
+    // this.list = {"Items":[{"date":"20-11-2022","from":"utrecht","status":"open","time":1200,"id":"t2","to":"amsterdam","seatsAvailable":2,"driverId":"p2"},{"date":"20-11-2022","from":"utrecht","status":"open","time":1200,"id":"t1","to":"amsterdam","seatsAvailable":1,"driverId":"p1"},{"date":"20-11-2022","from":"utrecht","status":"open","time":1200,"id":"t3","to":"amsterdam","seatsAvailable":3,"driverId":"p3"},{"date":"20-11-2022","from":"utrecht","status":"open","time":1200,"id":"t4","to":"amsterdam","seatsAvailable":4,"driverId":"p4"}],"Count":4,"ScannedCount":4}
+
+  },
+  methods: {
+    populate() {
+      let path = "https://lchaj9w7t3.execute-api.eu-west-1.amazonaws.com/dev/trips"
+     
     },
     onRowSelected(items) {
       this.selectedRide = items
