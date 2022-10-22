1936688efe2b1953d3d00f5ef29e1ba892434f5a
Juan Besselse <juan.besselse.navas@nl.abnamro.com>
2022-10-22 11:31:19 +0200

    fixed call

--- src/store/index.js
+++ src/store/index.js
@@ -175,7 +175,7 @@ export default new Vuex.Store({
 				})
 		},
 
-		findRide({ commit }, { payload, success }) {
+		findRide({ commit }, {  success }) {
 			let path = "https://lchaj9w7t3.execute-api.eu-west-1.amazonaws.com/dev/trips"
 			axios.get(path)
 				.then(function (response) {
